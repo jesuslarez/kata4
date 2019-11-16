9f01b6dda19107858153eeaaa881d81869332761
Familia <Familia@DESKTOP-U4D87FT>
2019-11-16 14:32:11 +0000

    Few corrections, check error messages and display

--- src/kata4/control/Kata4.java
+++ src/kata4/control/Kata4.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package kata4.control;
 
 import java.util.List;
@@ -14,15 +9,11 @@ import kata4.view.MailListReader;
 
 /**
  *
- * @author Usuario
+ * @author Jesus Larez
  */
 public class Kata4 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         String fileName = new String("email.txt"); 
         List<Mail> mailList = MailListReader.read(fileName); //I
         Histogram histogram = MailHistogramBuilder.build(mailList); //P
